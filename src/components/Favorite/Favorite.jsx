3f0c90442b2d50c61f73fc5aebf38e003624bba8
maxxxkam <muslimovmaxkam@gmail.com>
2025-01-16 18:20:02 +0500

    Add files via upload

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,39 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import s from './Favorite.module.scss';
-import { Link } from "react-router-dom";
-import Btn from "../btn/Btn"; // Ваши компоненты кнопок и другие элементы
+import { Link } from 'react-router-dom';
+// import Btn from "../Btn/Btn"; // Ваши компоненты кнопок и другие элементы
 
-import AOS from "aos";
-import "aos/dist/aos.css"; 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Btn from '../Btn/Btn';
 
 const Favorite = () => {
   const [favoriteCars, setFavoriteCars] = useState([]);
 
   // Загружаем избранные автомобили из localStorage
   useEffect(() => {
-    const storedFavorites = JSON.parse(localStorage.getItem("favoriteCars")) || [];
+    const storedFavorites =
+      JSON.parse(localStorage.getItem('favoriteCars')) || [];
     setFavoriteCars(storedFavorites);
   }, []);
 
   // Функция для добавления/удаления автомобиля из избранного
   const handleFavoriteClick = (car) => {
-    const storedFavorites = JSON.parse(localStorage.getItem("favoriteCars")) || [];
+    const storedFavorites =
+      JSON.parse(localStorage.getItem('favoriteCars')) || [];
     let updatedFavorites;
 
     if (storedFavorites.some((favCar) => favCar.id === car.id)) {
       // Убираем машину из избранного
-      updatedFavorites = storedFavorites.filter((favCar) => favCar.id !== car.id);
+      updatedFavorites = storedFavorites.filter(
+        (favCar) => favCar.id !== car.id
+      );
     } else {
       // Добавляем машину в избранное
       updatedFavorites = [...storedFavorites, car];
     }
 
-    localStorage.setItem("favoriteCars", JSON.stringify(updatedFavorites));
+    localStorage.setItem('favoriteCars', JSON.stringify(updatedFavorites));
     setFavoriteCars(updatedFavorites);
   };
 
   useEffect(() => {
-    AOS.init({ duration: 1000 }); 
+    AOS.init({ duration: 1000 });
   }, []);
 
   return (
@@ -45,15 +50,19 @@ const Favorite = () => {
         ) : (
           <div className={s.favorite_list}>
             {favoriteCars.map((car, index) => (
-              <div 
+              <div
                 key={car.id}
                 className={s.favorite_car}
-                data-aos="flip-right"  
-                data-aos-delay={index * 200} 
+                data-aos="flip-right"
+                data-aos-delay={index * 200}
               >
                 <div className={s.main_info}>
                   <div className={s.name_a_img}>
-                    <img src={car.image} alt={car.name} className={s.car_image} />
+                    <img
+                      src={car.image}
+                      alt={car.name}
+                      className={s.car_image}
+                    />
                     <h3 className={s.car_name}>{car.name}</h3>
                   </div>
                   <p>{car.about}</p>
@@ -64,13 +73,13 @@ const Favorite = () => {
                     <img
                       src={
                         favoriteCars.some((favCar) => favCar.id === car.id)
-                          ? "/like-2.svg"
-                          : "/like-1.svg"
+                          ? '/like-2.svg'
+                          : '/like-1.svg'
                       }
                       alt={
                         favoriteCars.some((favCar) => favCar.id === car.id)
-                          ? "Liked"
-                          : "Like"
+                          ? 'Liked'
+                          : 'Like'
                       }
                     />
                   </button>
@@ -78,8 +87,10 @@ const Favorite = () => {
                 <div className={s.line}></div>
                 <div className={s.add_info}>
                   <div className={s.prices}>
-                    <p className={s.price}>{car.price}</p>{" "}
-                    {car.oldPrice && <s className={s.oldPrice}>{car.oldPrice}</s>}
+                    <p className={s.price}>{car.price}</p>{' '}
+                    {car.oldPrice && (
+                      <s className={s.oldPrice}>{car.oldPrice}</s>
+                    )}
                   </div>
                   <Link to={`/carPage/${car.id}`}>
                     <Btn>Rent now</Btn>
